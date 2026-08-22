3bba668e9815dea360bcc2740e24c6bf3f40b94c
Andrew Fomin <andrfom1234567810@gmail.com>
2026-08-22 18:26:43 +0700

    Added basic settings menu

--- scenes/scripts/menu.gd
+++ scenes/scripts/menu.gd
@@ -1,6 +1,10 @@
 extends Control
 
 @onready var game_scene = "res://scenes/game.tscn"
+@onready var settings_menu = $SettingsMenu
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file(game_scene)
+
+func _on_settings_button_pressed() -> void:
+	settings_menu.show()
