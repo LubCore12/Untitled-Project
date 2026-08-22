3cd9af42d9b6b4f8c29e71dc5cc7416d159afa16
Andrew Fomin <andrfom1234567810@gmail.com>
2026-08-22 18:37:10 +0700

    Fixed player movement bug

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -5,28 +5,31 @@ extends CharacterBody2D
 @export var run_speed: float
 
 var direction: Vector2
-var current_speed = speed
+var current_speed: float
 
 signal player_attacked(damage)
+
+func _ready() -> void:
+	current_speed = speed
 
 func _physics_process(delta: float) -> void:
 	print(position)
 	get_input(delta)
 	move()
 	
+func get_input(_delta) -> void:
+	current_speed = speed
+	direction = Input.get_vector("left", "right", "up", "down")
+	
+	if Input.is_action_pressed("run"):
+		run()
+	
+	if Input.is_action_just_pressed("attack"):
+		player_attacked.emit()
+
 func move() -> void:
 	velocity = direction * current_speed
 	move_and_slide()
 
 func run() -> void:
 	current_speed = run_speed
-	
-func get_input(_delta) -> void:
-	if Input.is_action_pressed("run"):
-		run()
-	var x = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
-	var y = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_undo")
-	direction = Vector2(x,y).normalized()
-	
-	if Input.is_action_just_pressed("attack"):
-		player_attacked.emit()
